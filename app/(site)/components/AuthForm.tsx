853624a8378d6c0722bc4fcbb7fdf0b5149df802
ngothtsika <ngothtsikavivien@gmail.com>
2026-01-18 17:13:45 +0000

    remove google and git auth

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -3,11 +3,9 @@
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import { BsGithub, BsGoogle } from "react-icons/bs";
 
 import Input from "@/app/components/inputs/input";
 import Button from "@/app/components/Button";
-import AuthSocialButton from "./AuthSocialButton";
 import toast from "react-hot-toast";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -124,31 +122,6 @@ const AuthForm = () => {
             </Button>
           </div>
         </form>
-
-        <div className="mt-6">
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-gray-300" />
-            </div>
-
-            <div className="relative flex justify-center text-sm">
-              <span className="bg-white px-2 text-gray-500">
-                or continue with
-              </span>
-            </div>
-          </div>
-
-          <div className="mt-6 flex gap-2">
-            <AuthSocialButton
-              icon={BsGithub}
-              onClick={() => socialAction("github")}
-            />
-            <AuthSocialButton
-              icon={BsGoogle}
-              onClick={() => socialAction("google")}
-            />
-          </div>
-        </div>
 
         <div>
           <p className="mt-6 text-sm text-center gap-2 text-gray-500">
